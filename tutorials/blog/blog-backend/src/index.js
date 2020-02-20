a55932d056b97e885857cb45d29e2bff5c3aa6e5
sonyoon <sonyoon7@gmail.com>
2020-02-20 16:45:49 +0900

    router

--- tutorials/blog/blog-backend/src/index.js
+++ tutorials/blog/blog-backend/src/index.js
@@ -1,25 +1,39 @@
 const Koa = require("koa");
+const Router = require("koa-router");
 
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx, next) => {
-  console.log(ctx.url);
-  console.log(1);
-  if (ctx.query.authorized !== "1") {
-    ctx.status = 401;
-    return;
-  }
-  next();
+//라우터 설정
+router.get("/", ctx => {
+  ctx.body = "홈";
 });
 
-app.use((ctx, next) => {
-  console.log(2);
-  next();
+router.get("/about", ctx => {
+  ctx.body = "소개";
 });
 
-app.use(ctx => {
-  ctx.body = "hello world";
-});
+//app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+// app.use(async (ctx, next) => {
+//   console.log(ctx.url);
+//   console.log(1);
+//   if (ctx.query.authorized !== "1") {
+//     ctx.status = 401;
+//     return;
+//   }
+//   await next();
+//   console.log("END");
+// });
+
+// app.use((ctx, next) => {
+//   console.log(2);
+//   next();
+// });
+
+// app.use(ctx => {
+//   ctx.body = "hello world";
+// });
 
 app.listen(4000, () => {
   console.log("Listening to port 4000", "http://localhost:4000");
