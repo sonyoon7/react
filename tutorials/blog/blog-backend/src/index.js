fa7bbaef8dcfe69344c452a03bce1487b92d3e7e
sonyoon <sonyoon7@gmail.com>
2020-02-21 13:48:33 +0900

    koa-bodyparser

--- tutorials/blog/blog-backend/src/index.js
+++ tutorials/blog/blog-backend/src/index.js
@@ -1,17 +1,33 @@
 const Koa = require("koa");
 const Router = require("koa-router");
+const bodyParser = require("koa-bodyparser");
+
+const api = require("./api");
 
 const app = new Koa();
 const router = new Router();
 
-//라우터 설정
-router.get("/", ctx => {
-  ctx.body = "홈";
-});
+// //라우터 설정
+router.use("/api", api.routes());
 
-router.get("/about", ctx => {
-  ctx.body = "소개";
-});
+//라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+
+// router.get("/", ctx => {
+//   ctx.body = "홈";
+// });
+
+// router.get("/about/:name?", ctx => {
+//   const { name } = ctx.params;
+//   //name의 존재 유무에 따라 다른 결과 출력
+//   ctx.body = name ? `${name}의 소개` : `소개`;
+// });
+
+// router.get("/posts", ctx => {
+//   const { id } = ctx.query;
+//   //name의 존재 유무에 따라 다른 결과 출력
+//   ctx.body = id ? `포스트 #${id}` : `포스트 아이디가 없습니다`;
+// });
 
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
