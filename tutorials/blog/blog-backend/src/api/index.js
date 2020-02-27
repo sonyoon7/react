12b43fab3a427d935774cde93d1e492401829287
sonyoon <sonyoon7@gmail.com>
2020-02-27 17:06:19 +0900

    jwt token

--- tutorials/blog/blog-backend/src/api/index.js
+++ tutorials/blog/blog-backend/src/api/index.js
@@ -1,5 +1,6 @@
 import Router from "koa-router";
 import posts from "./posts";
+import auth from "./auth";
 
 const api = new Router();
 
@@ -8,6 +9,7 @@ const api = new Router();
 // });
 
 api.use("/posts", posts.routes());
+api.use("/auth", auth.routes());
 
 //라우터를 내보냅니다.
 export default api;
